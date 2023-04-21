fbf4bd723f0b0b5e22f35fcbde6cbd7391e92769
Boiko <Pavlo.Boiko@ukrnafta.com>
2023-04-21 22:41:58 +0300

    refactor(Правило '2.4 Атрибут 'type''): изменил доступ к елементам теперь будет  через class

--- app.js
+++ app.js
@@ -37,8 +37,9 @@ var createNewTaskElement=function(taskString){
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className="checkbox"
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="task task-inp";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
   editButton.className="edit";
@@ -82,7 +83,7 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector('input[type=text]');
+  var editInput=listItem.querySelector(".task-inp");
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit");
   var containsClass=listItem.classList.contains("editMode");
@@ -155,7 +156,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
 //select ListItems children
-  var checkBox=taskListItem.querySelector("input[type=checkbox]");
+  var checkBox=taskListItem.querySelector(".checkbox");
   var editButton=taskListItem.querySelector("button.edit");
   var deleteButton=taskListItem.querySelector("button.delete");
 
